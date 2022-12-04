df84d92a2cef27ee8aedbe74873aa413090a2dc8
Daniel C. Bucher <daniel.bucher88@gmail.com>
2022-12-04 16:05:46 -0300

    Posts: add post page

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,5 +1,16 @@
+import type { Post as PrismaPost } from "@prisma/client";
+
 import { prisma } from "~/db.server";
+
+export type Post = {
+  slug: string;
+  title: string;
+};
 
 export async function getPosts() {
   return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } })
 }
